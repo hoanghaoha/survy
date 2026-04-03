fb9dd28a99212491bf285b2a339ed594716527bf
hhhao <hhhao.vn@gmail.com>
2026-04-03 16:47:40 +0700

    chores: Add io.excel to public API

--- src/survy/__init__.py
+++ src/survy/__init__.py
@@ -1,7 +1,9 @@
 from survy.io.polars import read_polars
 from survy.io.csv import read_csv, to_csv
+from survy.io.excel import read_excel, to_excel
 from survy.io.json import read_json, to_json
 from survy.io.spss import to_spss
+
 
 from survy.survey.survey import Survey
 from survy.variable.variable import Variable, VarType
@@ -12,8 +14,10 @@ from survy.analyze.crosstab.functions import crosstab
 __all__ = [
     "read_polars",
     "read_csv",
+    "read_excel",
     "read_json",
     "to_csv",
+    "to_excel",
     "to_json",
     "to_spss",
     "Survey",
